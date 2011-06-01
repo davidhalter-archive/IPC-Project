0fa3ea50a9a73daac9fb24f8aa5aaaf6f6af91e9
David Halter <davidhalter88@gmail.com>
2011-06-01 20:02:09 +0200

    new semaphor param

--- sem.c
+++ sem.c
@@ -1,11 +1,11 @@
 
 #include <sys/sem.h>
 
-int sem_init(const char* key_name, int project_id){
+int sem_init(const char* key_name, int project_id, int new){
   key_t key = get_key(key_name, project_id);
   int flags;
 
-  if (1  > 0)
+  if (new > 0)
     flags = 0664 | IPC_CREAT;
   else
     flags = 0;
@@ -14,8 +14,10 @@ int sem_init(const char* key_name, int project_id){
   
   //sem_union.val = 0;
   semctl(sem_id, 0, SETVAL, 1); 
-  int val = semctl(sem_id, 0, GETVAL);
-  printf("test: %i\n", val);
+  if (new > 0){
+    int val = semctl(sem_id, 0, GETVAL);
+  }
+  //printf("test: %i\n", val);
   if (sem_id < 0) {
     printf("shm error\n");
     //exit(1);
