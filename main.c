5ad27fb21fe24f0fe061acc4b5a8f60a0fb1a4c2
Roman <r.fuerst@gmx.ch>
2011-06-03 03:19:10 +0200

    pointer fix

--- main.c
+++ main.c
@@ -145,7 +145,7 @@ pid_t init_display(char *argv[]){
 }
 
 pid_t init_control(pid_t pid_display, char *argv[]){
-  char * argv_control[4], argv_buf[sizeof(pid_t)];
+  char * argv_control[4], argv_buf[20];
   sprintf(argv_buf, "%d", pid_display);
   argv_control[0] = argv[0];
   argv_control[1] = argv[1];
